7505de4db2930e3b0d1d48bd0a383b7146604db6
Mayank <patelmayankce@gmail.com>
2020-04-25 00:23:33 +0530

    Updates

---
+++ precache-manifest.c2c21724772c024e67cbd998a1385d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb7e6d0f15ce51e38130be940f98e5c",
+    "url": "/react-redux-permission/index.html"
+  },
+  {
+    "revision": "5da94521dd9e145dbc58",
+    "url": "/react-redux-permission/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "0d30746c9a94bf3a01c4",
+    "url": "/react-redux-permission/static/js/2.e5ffc304.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-permission/static/js/2.e5ffc304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da94521dd9e145dbc58",
+    "url": "/react-redux-permission/static/js/main.9768bb15.chunk.js"
+  },
+  {
+    "revision": "54c343590b38369f1423",
+    "url": "/react-redux-permission/static/js/runtime-main.4e397914.js"
+  }
+]);
